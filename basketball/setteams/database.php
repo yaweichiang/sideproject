85d7f1a207f24fa3de69db72bca98cf9210dc89e
yaweichiang <yawei19901212@gmail.com>
2022-03-12 11:29:27 +0800

    add voice for scorelist

--- basketball/setteams/database.php
+++ basketball/setteams/database.php
@@ -1,7 +1,13 @@
 <?php
-$servername='localhost';
-$username='root';
-$password='0937513541';
-$dbname='basketball';
-$conn = new mysqli($servername,$username,$password,$dbname);
-?>
+// extension="mongodb.so"
+// $servername='localhost';
+// $username='root';
+// $password='0937513541';
+// $dbname='basketball';
+// $conn = new mysqli($servername,$username,$password,$dbname);
+// require '/usr/local/Cellar/composer/2.2.7/bin/vendor/autoload.php';
+// /usr/local/Cellar/composer/2.2.7/bin/vendor/autoload.php
+$client = new MongoDB\Client('mongodb+srv://web_app:webapp@webserver.yqgg6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+$conn = $client->basketball;
+//  phpinfo();  
+?> 
--- basketball/setteams/singup.php
+++ basketball/setteams/singup.php
@@ -5,25 +5,32 @@ if($conn->connect_error){
     die("Connection Failed".$conn->connect_error);
 }
 
+echo "link";
+
 //GET 前端傳回資料
 $nickname = $_GET['nickname'];
-$sql="SELECT nickname FROM player WHERE nickname = '".$nickname."'";
-$result = $conn->query($sql);
-if($result->num_rows>0){       
-        $conn->close();
-        echo 'same_nick_name';
-}else{
-    $sql = "INSERT INTO player(nickname) VALUES ('".$nickname."')";
-    if ($conn->query($sql) === TRUE) {
-        // 資料庫寫入成功 關閉連線
-        $conn->close();
-    } else {
-        // 資料庫寫入失敗 關閉連線 回傳失敗訊息
-        $msg = "Error: " . $sql . "<br>" . $conn->error;
-        $conn->close();
-        echo $msg;
-    }
-}
+$insertOneResult = $conn->player->insertOne([
+    'nickname' => $nickname ,
+    'type' => 'online',
+
+]);
+// $sql="SELECT nickname FROM player WHERE nickname = '".$nickname."'";
+// $result = $conn->query($sql);
+// if($result->num_rows>0){       
+//         $conn->close();
+//         echo 'same_nick_name';
+// }else{
+//     $sql = "INSERT INTO player(nickname) VALUES ('".$nickname."')";
+//     if ($conn->query($sql) === TRUE) {
+//         // 資料庫寫入成功 關閉連線
+//         $conn->close();
+//     } else {
+//         // 資料庫寫入失敗 關閉連線 回傳失敗訊息
+//         $msg = "Error: " . $sql . "<br>" . $conn->error;
+//         $conn->close();
+//         echo $msg;
+//     }
+// }
 
 
 
